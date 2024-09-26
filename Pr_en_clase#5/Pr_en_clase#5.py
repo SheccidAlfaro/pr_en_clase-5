02bffc65acff4bda2b3f8e61c7da344871464149
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-25 20:10:52 -0600

    Add files via upload

---
+++ Pr_en_clase#5/Pr_en_clase#5.py
@@ -0,0 +1,16 @@
+print("")
+#DAr una instruccion y explicar la tematica
+print("Adivinar el numero Alfaro Ibarra Sheccid 3W")
+#Dar una instruccion
+print("Hay un numero correcto del 1 a l5.")
+print("No es numero primo.")
+#se declara variable
+n=print(input("Incerta el numero:"))
+#Utilizar if elsee para poner una condicion
+if n == 10:
+    print("es correcto")
+else:
+    print("Has fallado.")
+print("")
+
+
